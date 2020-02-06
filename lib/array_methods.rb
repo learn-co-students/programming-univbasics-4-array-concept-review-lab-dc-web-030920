ad39566d69619c6a6116492326ea0a060b1a8103
Nduka-Eze Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-06 04:35:28 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,37 @@
+#Loop through array
+#to find position value
+
+#Find maximum value in array
+#Find minimum value in array
+
+#Loop over provided array
+ 
+ 
+scale = [2,3,4,5]
+scale_2 = [6,5,4,3]
+hill = [1,2,3,4,5,4,3,2,1]
+valley = [5,4,3,2,1,0,1,2,3,4,5]
+zig_zag = [500,4,1000,5,250]
+
+
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  
+  1.times do |value_to_find|
+    array.index(value_to_find)
+  end
+  array.index(value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  1.times do
+    array.max
+  end
+  array.max
 end
 
 def find_min_value(array)
-  # Add your solution here
+  1.times do
+    array.min
+  end
+  array.min
 end
